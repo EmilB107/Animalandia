2df0f9ab42121ee1300d50dd362efd0cfe6ef825
Bonjing <johnemilbragais@gmail.com>
2025-01-23 21:23:14 +0800

    Create Animals.cs

---
+++ Animalandia/Models/Animals.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Animalandia.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string SoundFilePath { get; set; }
+        public string ImageFilePath { get; set; }
+
+        public virtual string Introduction()
+        {
+            return "This is an animal.";
+        }
+
+        public virtual string Description()
+        {
+            return "Animals are multicellular, eukaryotic organisms of the kingdom Animalia.";
+        }
+    }
+}
